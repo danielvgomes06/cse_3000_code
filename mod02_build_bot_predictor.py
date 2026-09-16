3f44dfe356c433e032944cf5cda632bf35a09e99
danielvgomes06 <danielgomes29@icloud.com>
2026-09-16 14:01:39 -0400

    setup bot predictor

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -11,7 +11,7 @@ def train_model(X, y, seed=seed):
     """
     model = GradientBoostingClassifier(
         learning_rate=0.1,
-        n_estimators=100,
+        n_estimators=500,
         max_depth=8,
         subsample=1,
         min_samples_leaf=1,
